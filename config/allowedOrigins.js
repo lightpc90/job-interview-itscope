9eed1c9437314cc4de4289274c869333d37a7943
lightpc90 <afoageektech@gmail.com>
2023-09-29 10:35:32 +0100

    included app host in cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "http://node-api-env.eba-vcgr2nt7.eu-west-2.elasticbeanstalk.com/",
+  "http://marketplace-env-1.eba-7fn667pw.eu-west-2.elasticbeanstalk.com/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
