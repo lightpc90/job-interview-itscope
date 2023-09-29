b079e82af41e534a8bfd9a3496aad173953bc65e
lightpc90 <afoageektech@gmail.com>
2023-09-29 03:48:32 +0100

    updated my cors to allow my eb

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
+  "http://node-api-env.eba-vcgr2nt7.eu-west-2.elasticbeanstalk.com/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
